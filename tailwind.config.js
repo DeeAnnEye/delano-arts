37f12999cdfad90b407efd7c1ca2052b7f6499f2
DeeAnnEye <danielashulamithjustin@gmail.com>
2022-08-25 22:37:24 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        amber: colors.amber,
+        pink: colors.pink,
+        emerald: colors.emerald,
+        violet: colors.violet,
+        rose: colors.rose,
+        purple: colors.purple,
+        cyan: colors.cyan,
+      }
+    },
+  },
+  plugins: [],
+}
